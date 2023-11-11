29f25cd2dc240f48978e83e4bd6c213764d724aa
moukouba <m_moukouba@yahoo.fr>
2023-11-11 05:16:18 +0100

    project structure and requirements added

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Object-Detection-on-Low-Visibility-and-IR-Images-YOLO-NAS"
+AUTHOR_USER_NAME = "Moukouba"
+SRC_REPO = "yoloNasClassifier"
+AUTHOR_EMAIL = "m_moukouba@yahoo.fr"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A YOLO-NAS based robust object detector on RGB and Thermal images",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
